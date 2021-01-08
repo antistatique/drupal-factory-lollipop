36aa89608e2887d5456ec6c92a26c69277cded61
Kevin Wenger <wenger.kev@gmail.com>
2021-01-08 15:02:59 +0100

    add Taxonomy Vocabulary factory

---
+++ src/FactoryType/VocabularyFactoryType.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\factory_lollipop\FactoryType;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\taxonomy\VocabularyInterface;
+use Drupal\Tests\RandomGeneratorTrait;
+
+/**
+ * Creates Drupal Vocabulary for use in tests.
+ */
+class VocabularyFactoryType implements FactoryTypeInterface {
+  use RandomGeneratorTrait;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface|null
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Sets the entity type manager.
+   *
+   * This is only called when the factory is instantiated.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The new entity type manager.
+   */
+  public function setEntityTypeManager(EntityTypeManagerInterface $entity_type_manager): void {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function shouldApply(string $type): bool {
+    return $type === 'vocabulary';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIdentifier(object $factory_object) {
+    /** @var \Drupal\taxonomy\VocabularyInterface $factory_object */
+    return $factory_object->id();
+  }
+
+  /**
+   * Create or return and existing and persisted Vocabulary.
+   *
+   * @param object|null $attributes
+   *   Vocabulary attributes to use for creation.
+   *
+   * @return \Drupal\taxonomy\VocabularyInterface
+   *   The newly created or already existing Vocabulary.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   *
+   * @internal
+   */
+  public function create(?object $attributes = NULL): VocabularyInterface {
+    // Load the storage at the last moment to prevent requiring Node module
+    // on shouldApply phase.
+    $vocabulary_storage = $this->entityTypeManager->getStorage('taxonomy_vocabulary');
+
+    $attributes = (array) $attributes;
+
+    $vid = $attributes['vid'] ?? mb_strtolower($this->randomMachineName());
+    $name = $attributes['name'] ?? $this->randomString();
+
+    // Prevent creation of already existing Vocabulary.
+    $vocabulary = $vocabulary_storage->load($vid);
+    if ($vocabulary) {
+      return $vocabulary;
+    }
+
+    // Create a vocabulary for testing.
+    $values = $attributes ?? [];
+    $vocabulary = $vocabulary_storage->create($values + [
+      'vid' => $vid,
+      'name' => $name,
+    ]);
+    $vocabulary->save();
+    return $vocabulary;
+  }
+
+}
---
+++ tests/modules/factory_lollipop_test/tests/src/Kernel/VocabularyFactoryTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\Tests\factory_lollipop_test\Kernel;
+
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\taxonomy\VocabularyInterface;
+use Drupal\Tests\factory_lollipop\Kernel\LollipopKernelTestBase;
+
+/**
+ * Example of Factory Lollipop usage for Vocabulary.
+ *
+ * @group factory_lollipop
+ * @group factory_lollipop_example
+ */
+class VocabularyFactoryTest extends LollipopKernelTestBase {
+
+  /**
+   * The Factory Lollipop fixture factory.
+   *
+   * @var \Drupal\factory_lollipop\FixtureFactory
+   */
+  protected $factoryLollipop;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'taxonomy',
+    'factory_lollipop_test',
+  ];
+
+  /**
+   * Ensure a defined node type can be created.
+   *
+   * @covers \Drupal\factory_lollipop\FixtureFactory::loadDefinitions
+   * @covers \Drupal\factory_lollipop\FixtureFactory::define
+   * @covers \Drupal\factory_lollipop\FixtureFactory::create
+   * @covers \Drupal\factory_lollipop\FactoryType\VocabularyFactoryType::create
+   */
+  public function testDefine(): void {
+    $this->factoryLollipop->loadDefinitions(['vocabulary']);
+
+    // Ensure the defined vocabulary is not created before call of ::create().
+    $vocabulary = Vocabulary::load('tags');
+    self::assertNull($vocabulary);
+
+    /** @var \Drupal\taxonomy\VocabularyInterface $vocabulary */
+    $vocabulary = $this->factoryLollipop->create('vocabulary_tags');
+    self::assertInstanceOf(VocabularyInterface::class, $vocabulary);
+    self::assertEquals('tags', $vocabulary->id());
+  }
+
+}
