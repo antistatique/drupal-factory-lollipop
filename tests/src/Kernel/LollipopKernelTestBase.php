3c5d083dbc055f495839ceb1e7727b95f6b6f2ec
Kevin Wenger <wenger.kev@gmail.com>
2021-01-08 13:52:02 +0100

    provides a base class for Factory Lollipop kernel tests

---
+++ tests/src/Kernel/LollipopKernelTestBase.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\Tests\factory_lollipop\Kernel;
+
+use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
+
+/**
+ * Provides a base class for Factory Lollipop kernel tests.
+ */
+abstract class LollipopKernelTestBase extends EntityKernelTestBase {
+
+  /**
+   * The Factory Lollipop fixture factory.
+   *
+   * @var \Drupal\factory_lollipop\FixtureFactory
+   */
+  protected $factoryLollipop;
+
+  /**
+   * {@inheritdoc}
+   *
+   * Note that when a child class declares its own $modules list, that list
+   * doesn't override this one, it just extends it.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'factory_lollipop',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->factoryLollipop = $this->container->get('factory_lollipop.fixture_factory');
+  }
+
+}
